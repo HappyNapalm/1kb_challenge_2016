d608ac693e91064c5b51f823a33edebd8222dcd7
HappyNapalm <maevestring@gmail.com>
2017-05-13 01:47:46 -0700

    Add files via upload

---
+++ sketch_dec11b.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+
+int var = 0;
+Servo servo_01;
+int i = 0;
+
+void setup() {
+  pinMode(9,OUTPUT);
+  Serial.begin(9600);
+  servo_01.attach(9);
+
+}
+
+void loop() {
+  for (i=0;i<160;i++){
+    servo_01.write(i);
+    Serial.println(i);
+    delay(10);
+  }
+  for (i=160;i>0;i--){
+    servo_01.write(i);
+    Serial.println(i);
+    delay(10);
+  }
+  servo_01.write(90);
+  delay(500);
+  
+
+}
---
+++ timer_testing.ino
@@ -0,0 +1,48 @@
+uint8_t safe_time = 0;
+
+void setup() {
+  noInterrupts();
+  //Look up data sheets for more information
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A = 31250;            // compare match register 16MHz/256/[2Hz]
+  //0.5 timer
+  TCCR1B |= (1 << WGM12);   // CTC mode
+  TCCR1B |= (1 << CS12);    // 256 prescaler 
+  TIMSK1 |= (1 << OCIE1A);  // enable timer compare interrupt
+  interrupts();
+
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+
+  TCNT1H = 0;//clears timer High Byte
+  safe_time = 0;//reset time counter
+  TCNT1L = 0;//clears timer Low Byte 
+}
+
+ISR(TIMER1_COMPA_vect){//timer 1
+  safe_time++;
+}
+
+void loop() {
+  if (safe_time%2 == 1){
+    digitalWrite(13,HIGH);
+    digitalWrite(12,LOW);
+  }
+  else if (safe_time%3 == 1){
+    digitalWrite(12,HIGH);
+    digitalWrite(13,LOW);
+  }
+  else{
+    digitalWrite(13,LOW);
+    digitalWrite(12,LOW);
+  }
+}
+
+
+
+
+
